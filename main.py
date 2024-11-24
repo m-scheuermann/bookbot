ccb818c96c81e0a487f526bd8f4270b9c622d35c
m-scheuermann <m.scheuermann23@gmail.com>
2024-11-24 15:05:31 +0100

    structured report of amount of words and characters

--- main.py
+++ main.py
@@ -2,18 +2,27 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     number_of_words = count_words(text)
-    print(f"This book has {number_of_words} words.")
+    print(f"--- Begin report of {book_path} ---")
+    print(f"This document has {number_of_words} words.")
+    print()
     number_of_characters = count_characters(text)
-    print("This book has a lot of characters! Here's a list of them and how many there are:\n", number_of_characters, sep="")
+    characters_as_list = dict_to_list(number_of_characters)
+    characters_as_list.sort(reverse=True, key=sort_on)
+    for character in characters_as_list:
+        print(f"The '{character["character"]}' character was found {character["amount"]} times")
+    print("--- End report ---")
+
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
+
 def count_words(text):
     words = text.split()
     words_number = len(words)
     return words_number
+
 
 def count_characters(text):
     lowered_text = text.lower()
@@ -21,8 +30,18 @@ def count_characters(text):
     for character in lowered_text:
         if character in characters:
             characters[character] += 1
-        else:
+        elif character.isalpha() == True:
             characters[character] = 1
     return characters
+
+
+def dict_to_list(dict):
+    dict_as_list = [{"character": key, "amount" : value} for key, value in dict.items()]
+    return dict_as_list
+
+
+def sort_on(dict):
+    return dict["amount"]
+
 
 main()
