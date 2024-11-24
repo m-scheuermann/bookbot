b65b6ae1c843a96d9cc4afeb3c4e8821ebdf8870
m-scheuermann <m.scheuermann23@gmail.com>
2024-11-24 14:33:13 +0100

    count the characters

--- main.py
+++ main.py
@@ -2,16 +2,27 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     number_of_words = count_words(text)
-    print(number_of_words)
-
+    print(f"This book has {number_of_words} words.")
+    number_of_characters = count_characters(text)
+    print("This book has a lot of characters! Here's a list of them and how many there are:\n", number_of_characters, sep="")
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
-def count_words(text_string):
-    words = text_string.split()
+def count_words(text):
+    words = text.split()
     words_number = len(words)
     return words_number
+
+def count_characters(text):
+    lowered_text = text.lower()
+    characters = {}
+    for character in lowered_text:
+        if character in characters:
+            characters[character] += 1
+        else:
+            characters[character] = 1
+    return characters
 
 main()
